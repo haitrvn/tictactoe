223a915f9abac47034de2237ceeca7b5e3c0de4b
Hai Tran <haitrvn@gmail.com>
2025-08-28 22:44:44 +0700

    Path

--- features/splash/src/commonMain/kotlin/com/haitrvn/splash/DrawPart2.kt
+++ features/splash/src/commonMain/kotlin/com/haitrvn/splash/DrawPart2.kt
@@ -1,13 +1,11 @@
 package com.haitrvn.splash
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -16,231 +14,103 @@ fun Drop(
     modifier: Modifier = Modifier,
     padding: Dp
 ) {
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        val padding = padding.toPx()
-        drawRect(Color.Red)
-        translate(padding, padding) {
-            drawPath(
-                color = Color.Black,
-                path = Path().apply {
-                    val width = size.width - padding * 2
-                    val height = size.height - padding * 2
-                    moveTo(padding, padding)
-                    createCubicList(
-                        width = width,
-                        height = height,
-                        dropSize = 60.dp.toPx(),
-                        cornerSize = 40.dp.toPx(),
-                        offset = 80.dp.toPx(),
-                        side = Side.BOTTOM,
-                    ).forEach {
-                        moveToAndCubicTo(it)
-                    }
-                }
-            )
-        }
+    Canvas(modifier = modifier) {
+        val path = generateCubic(
+            side = Side.TOP,
+            width = size.width,
+            height = size.height,
+            dropSize = 60.dp.toPx(),
+            cornerSize = 60.dp.toPx(),
+            offset = 200.dp.toPx(),
+        )
+        drawPath(path, Color.Red, style = Stroke(width = 5f))
     }
 }
 
-fun createCubicList(
+enum class Side {
+    TOP, BOTTOM, LEFT, RIGHT
+}
+
+
+/**
+ * Sample path:
+ * M 10 20 C 10 15 15 10 20 10 L 35 10 C 38 10 40 8 40 5 A 1 1 0 0 1 50 5 C 50 8 52 10 55 10
+ * L 70 10 C 75 10 80 15 80 20 L 80 35 C 80 38 82 40 85 40 A 1 1 0 0 1 85 50 C 82 50 80 52 80 55
+ * L 80 70 C 80 75 75 80 70 80 L 55 80 C 52 80 50 82 50 85 A 1 1 0 0 1 40 85 C 40 82 38 80 35 80
+ * L 20 80 C 15 80 10 75 10 70 L 10 55 C 10 52 8 50 5 50 A 1 1 0 0 1 5 39 C 8 39 10 37 10 35 L 10 20 Z
+ */
+fun generateCubic(
+    side: Side,
     width: Float,
     height: Float,
     dropSize: Float,
     cornerSize: Float,
     offset: Float,
-    side: Side,
-): List<Cubic> {
-    val top = if (side == Side.TOP) dropSize else 0f
-    val bottom = if (side == Side.BOTTOM) height - dropSize else height
-    val left = if (side == Side.LEFT) dropSize else 0f
-    val right = if (side == Side.RIGHT) width - dropSize else width
-
-    val topCorner = if (side == Side.TOP) {
-        (top + offset).coerceAtMost(top + cornerSize)
+): Path {
+    val dropXY1 = offset
+    val dropXY2 = offset + dropSize
+    val halfDropSize = dropSize / 2
+    val topDropY = if (side == Side.TOP) halfDropSize else 0f
+    val topY = if (side == Side.TOP) dropSize else 0f
+    val topCornerX1 = if (offset >= dropSize + cornerSize && side == Side.TOP) {
+        cornerSize
     } else {
-        top + cornerSize
+        offset * (cornerSize / (cornerSize + dropSize))
     }
-    val bottomCorner = if (side == Side.BOTTOM) {
-        (bottom - offset).coerceAtLeast(bottom - cornerSize)
+    val topDropX1 = if (offset > dropSize + cornerSize) {
+        offset - halfDropSize
     } else {
-        bottom - cornerSize
-    }
-    val leftCorner = if (side == Side.LEFT) {
-        (left + offset).coerceAtMost(left + cornerSize)
-    } else {
-        left + cornerSize
-    }
-    val rightCorner = if (side == Side.RIGHT) {
-        (right - offset).coerceAtLeast(right - cornerSize)
-    } else {
-        right - cornerSize
+        topCornerX1
     }
 
-    val maxFootDropHeight = dropSize / 3
+    val topCornerX2 = if (width - offset - dropSize >= cornerSize + dropSize && side == Side.TOP) {
+        width - cornerSize
+    } else {
+        (width - offset - dropSize) * (cornerSize / (cornerSize + dropSize))
+    }
+    val topDropX2 = if (width - offset - dropSize > cornerSize + dropSize) {
+        offset + dropSize + halfDropSize
+    } else {
+        topCornerX2
+    }
+    val leftDropX1 = if (side == Side.LEFT) width - halfDropSize else 0f
+    val leftY = if (side == Side.LEFT) width - dropSize else 0f
 
-    val topFoot = if (side == Side.TOP) {
-        top - maxFootDropHeight
+    val leftCornerY1 = if (offset >= dropSize + cornerSize && side == Side.LEFT) {
+        cornerSize
     } else {
-        top
+        offset * (cornerSize / (cornerSize + dropSize))
     }
-    val bottomFoot = if (side == Side.BOTTOM) {
-        bottom + maxFootDropHeight
+    val leftDropY1 = if (offset > dropSize + cornerSize) {
+        offset - halfDropSize
     } else {
-        bottom
-    }
-    val leftFoot = if (side == Side.LEFT) {
-        left - maxFootDropHeight
-    } else {
-        left
-    }
-    val rightFoot = if (side == Side.RIGHT) {
-        right + maxFootDropHeight
-    } else {
-        right
-    }
-
-    val topDropHeader = if (side == Side.TOP) {
-        top - dropSize
-    } else {
-        top
-    }
-    val bottomDropHeader = if (side == Side.BOTTOM) {
-        bottom + dropSize
-    } else {
-        bottom
-    }
-    val leftDropHeader = if (side == Side.LEFT) {
-        left - dropSize
-    } else {
-        left
-    }
-    val rightDropHeader = if (side == Side.RIGHT) {
-        right + dropSize
-    } else {
-        right
+        leftCornerY1
     }
 
-    val offset1 = offset.coerceAtMost(width - dropSize)
-    val offset2 = offset1 + dropSize
+    val leftCornerX2 = if (width - offset - dropSize >= cornerSize + dropSize && side == Side.TOP) {
+        cornerSize
+    } else {
+        (topY - offset - dropSize) * (cornerSize / (cornerSize + dropSize))
+    }
+    return Path().apply {
+        drawVerticalLine(topCornerX1, height)
+        drawVerticalLine(topCornerX2, height)
+        drawVerticalLine(topDropX1, height)
+        drawVerticalLine(topDropX2, height)
+        drawVerticalLine(dropXY1, height)
+        drawVerticalLine(dropXY2, height)
 
-    return listOf(
-        //TOP_LEFT
-        Cubic(
-            startPoint = Point(left, topCorner),
-            endPoint = Point(leftCorner, top),
-            controlPoint1 = Point(left, topCorner - (topCorner - top) / 2),
-            controlPoint2 = Point(leftCorner - (leftCorner - left) / 2, top),
-        ),
-        //TOP_DROP_FOOT_1
-        Cubic(
-            startPoint = Point(leftCorner, top),
-            endPoint = Point(offset1, topFoot),
-            controlPoint1 = Point(offset1, top),
-            controlPoint2 = Point(offset1, top),
-        ),
-        //TOP_HEADER_DROP
-        Cubic(
-            startPoint = Point(offset1, topFoot),
-            endPoint = Point(offset2, topFoot),
-            controlPoint1 = Point(offset1, topDropHeader),
-            controlPoint2 = Point(offset2, topDropHeader),
-        ),
-        //TOP_DROP_FOOT_2
-        Cubic(
-            startPoint = Point(offset2, topFoot),
-            endPoint = Point(rightCorner, top),
-            controlPoint1 = Point(offset2, top),
-            controlPoint2 = Point(offset2, top),
-        ),
-        //TOP_RIGHT
-        Cubic(
-            startPoint = Point(rightCorner, top),
-            endPoint = Point(right, topCorner),
-            controlPoint1 = Point(rightCorner + (right - rightCorner) / 2, top),
-            controlPoint2 = Point(right, topCorner - (topCorner - top) / 2),
-        ),
-        //RIGHT_DROP_FOOT_1
-        Cubic(
-            startPoint = Point(right, topCorner),
-            endPoint = Point(rightFoot, offset1),
-            controlPoint1 = Point(right, offset1),
-            controlPoint2 = Point(right, offset1),
-        ),
-        //RIGHT_HEADER_DROP
-        Cubic(
-            startPoint = Point(rightFoot, offset1),
-            endPoint = Point(rightFoot, offset2),
-            controlPoint1 = Point(rightDropHeader, offset1),
-            controlPoint2 = Point(rightDropHeader, offset2),
-        ),
-        //RIGHT_DROP_FOOT_2
-        Cubic(
-            startPoint = Point(rightFoot, offset2),
-            endPoint = Point(right, bottomCorner),
-            controlPoint1 = Point(right, offset2),
-            controlPoint2 = Point(right, offset2),
-        ),
-        //BOTTOM_RIGHT
-        Cubic(
-            startPoint = Point(right, bottomCorner),
-            endPoint = Point(rightCorner, bottom),
-            controlPoint1 = Point(right, bottomCorner + (bottom - bottomCorner) / 2),
-            controlPoint2 = Point(rightCorner + (right - rightCorner) / 2, bottom)
-        ),
-        //BOTTOM_DROP_FOOT_1
-        Cubic(
-            startPoint = Point(rightCorner, bottom),
-            endPoint = Point(offset2, bottomFoot),
-            controlPoint1 = Point(offset2, bottom),
-            controlPoint2 = Point(offset2, bottom),
-        ),
-        //BOTTOM_HEADER_DROP
-        Cubic(
-            startPoint = Point(offset2, bottomFoot),
-            endPoint = Point(offset1, bottomFoot),
-            controlPoint1 = Point(offset2, bottomDropHeader),
-            controlPoint2 = Point(offset1, bottomDropHeader),
-        ),
-        //BOTTOM_DROP_FOOT_2
-        Cubic(
-            startPoint = Point(offset1, bottomFoot),
-            endPoint = Point(leftCorner, bottom),
-            controlPoint1 = Point(offset1, bottom),
-            controlPoint2 = Point(offset1, bottom),
-        ),
-        //BOTTOM_LEFT
-        Cubic(
-            startPoint = Point(leftCorner, bottom),
-            endPoint = Point(left, bottomCorner),
-            controlPoint1 = Point(leftCorner - (leftCorner - left) / 2, bottom),
-            controlPoint2 = Point(left, bottomCorner + (bottom - bottomCorner) / 2)
-        ),
-        //LEFT_DROP_FOOT_1
-        Cubic(
-            startPoint = Point(left, bottomCorner),
-            endPoint = Point(leftFoot, offset2),
-            controlPoint1 = Point(left, offset2),
-            controlPoint2 = Point(left, offset2),
-        ),
-        //LEFT_HEADER_DROP
-        Cubic(
-            startPoint = Point(leftFoot, offset2),
-            endPoint = Point(leftFoot, offset1),
-            controlPoint1 = Point(leftDropHeader, offset2),
-            controlPoint2 = Point(leftDropHeader, offset1),
-        ),
-        //LEFT_DROP_FOOT_2
-        Cubic(
-            startPoint = Point(leftFoot, offset1),
-            endPoint = Point(left, topCorner),
-            controlPoint1 = Point(left, offset1),
-            controlPoint2 = Point(left, offset1),
-        ),
-    )
+        drawHorizontalLine(topDropY, width)
+        drawHorizontalLine(topY, width)
+    }
 }
 
+fun Path.drawVerticalLine(x: Float, canvasHeight: Float) {
+    moveTo(x, 0f)
+    lineTo(x, canvasHeight)
+}
 
-fun Path.moveToAndCubicTo(cubic: Cubic) {
+fun Path.cubicToPath(cubic: Cubic) {
     lineTo(cubic.startPoint.x, cubic.startPoint.y)
     cubicTo(
         cubic.controlPoint1.x,
@@ -248,38 +118,11 @@ fun Path.moveToAndCubicTo(cubic: Cubic) {
         cubic.controlPoint2.x,
         cubic.controlPoint2.y,
         cubic.endPoint.x,
-        cubic.endPoint.y,
-    )
-//    moveTo(cubic.startPoint.x, cubic.startPoint.y)
-//    drawDot(cubic.startPoint.x, cubic.startPoint.y)
-//    drawDot(cubic.controlPoint1.x, cubic.controlPoint1.y)
-//    drawDot(cubic.controlPoint2.x, cubic.controlPoint2.y)
-//    drawDot(cubic.endPoint.x, cubic.endPoint.y)
-}
-
-private fun Path.drawDot(
-    x: Float,
-    y: Float,
-    dotRadius: Float = 2f
-) {
-    addOval(
-        Rect(
-            left = x - dotRadius,
-            top = y - dotRadius,
-            right = x + dotRadius,
-            bottom = y + dotRadius
-        )
+        cubic.endPoint.y
     )
 }
 
 data class Point(val x: Float, val y: Float)
-
-enum class Side {
-    TOP,
-    BOTTOM,
-    LEFT,
-    RIGHT,
-}
 
 data class Cubic(
     val startPoint: Point,
@@ -287,3 +130,8 @@ data class Cubic(
     val controlPoint1: Point,
     val controlPoint2: Point,
 )
+
+fun Path.drawHorizontalLine(y: Float, canvasWidth: Float) {
+    moveTo(0f, y)
+    lineTo(canvasWidth, y)
+}
