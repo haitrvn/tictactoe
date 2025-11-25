919de78e0465646cd4d930e3b79f4e0be58ed9a8
Hai Tran <haitrvn@gmail.com>
2025-11-25 20:54:22 +0700

    Im tired

--- core/config/build.gradle.kts
+++ core/config/build.gradle.kts
@@ -20,40 +20,7 @@ kotlin {
                 implementation(projects.core)
                 implementation(libs.kotlin.stdlib)
                 implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.components.resources)
-                implementation(compose.components.uiToolingPreview)
-                implementation(libs.androidx.lifecycle.runtime.compose)
-                implementation(libs.koin.compose)
-                implementation(libs.coil.core)
-                implementation(libs.coil.compose)
-                implementation(libs.coil.network.core)
-                implementation(libs.coil.network.ktor)
-                implementation(libs.coil.network.cache.control)
-                implementation(libs.ktor.client.core)
             }
-        }
-
-        commonTest {
-            dependencies {
-                implementation(libs.kotlin.test)
-            }
-        }
-
-        androidMain.dependencies {
-            implementation(libs.ktor.client.okhttp)
-        }
-
-        jvmMain.dependencies {
-            implementation(libs.ktor.client.okhttp)
-            implementation("org.jetbrains.compose.ui:ui-tooling-preview:1.8.0")
-        }
-
-        iosMain.dependencies {
-            implementation(libs.ktor.client.darwin)
-        }
-        wasmJsMain.dependencies {
-            implementation("io.ktor:ktor-client-js:3.2.1")
         }
     }
 }
--- data/src/commonMain/kotlin/com/haitrvn/data/SplashRepositoryImpl.kt
+++ data/src/commonMain/kotlin/com/haitrvn/data/SplashRepositoryImpl.kt
@@ -9,10 +9,7 @@ class SplashRepositoryImpl(
     private val localDateSource: LocalSplashDateSource,
 ) : SplashRepository {
     override suspend fun invoke(): SplashContent {
-        return SplashContent(
-            splashScreenUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsK8g4zhtM_TszTYUho3dMZ2r3uwS-WLL4mA&s",
-            tutorialScreenUrl = fakeData
-        )
+        TODO()
     }
 }
 
@@ -22,22 +19,3 @@ class RemoteSplashDateSourceImpl : RemoteSplashDateSource
 interface LocalSplashDateSource
 
 class LocalSplashDateSourceImpl : LocalSplashDateSource
-
-val fakeData =
-    listOf(
-        PageContent(
-            "Share Your Recipes",
-            "Share your favorite recipes with the world!blablablablablablablablablablablablablablablablablabla",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsK8g4zhtM_TszTYUho3dMZ2r3uwS-WLL4mA&s"
-        ),
-        PageContent(
-            "Discover New Flavors",
-            "Explore a wide variety of cuisines and dishes.blablablablablablablablablablablablablablablablablablablablablabla",
-            "https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
-        ),
-        PageContent(
-            "Cook Like a Pro",
-            "Learn new cooking techniques and tips.blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla",
-            "https://vietop.edu.vn/wp-content/uploads/2023/07/idioms-chu-de-food.jpg"
-        ),
-    )
